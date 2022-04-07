61008d964a908d82240b79cf6e2d3e7827db55a0
Matias Garay <maty1173@gmail.com>
2022-04-06 21:16:42 -0300

    arregla img

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const btnSend= document.querySelector(".btnSend");
 const pokeInfo= document.querySelector(".pokeInfo");
 const pokeName= document.querySelector(".pokeInfo__name");
+const pokeNumber=document.querySelector(".pokeInfo__number");
 const pokeImg= document.querySelector(".pokeInfo__img")
 const input= document.querySelector("#poke");
+
+function capitalizeFirstLetter(string) {
+   return string.charAt(0).toUpperCase() + string.slice(1);
+ }
+
 
 btnSend.addEventListener("click", ()=>{
     const search= input.value.toLowerCase();
@@ -10,8 +16,9 @@ btnSend.addEventListener("click", ()=>{
         axios.get(`https://pokeapi.co/api/v2/pokemon/${search}`)
          .then((res) => {
             console.log(res);
-            pokeName.textContent= res.data.name;
+            pokeName.textContent= capitalizeFirstLetter(res.data.name);
             pokeImg.src=res.data.sprites.front_default;
+            pokeNumber.textContent= `Numero en la pokedex ${res.data.id}`
             err="no se ingresó ningun nombre"
          })
          .catch((err) => {
